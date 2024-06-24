78816206b4870503e4c8f4279305a919a179f114
Osama Hussein <osamahusseinn2@gmail.com>
2024-06-24 16:38:46 -0400

    I added a set timeout function to wait for a certain amount of time to close the program!

--- JavascriptPractice/app.js
+++ JavascriptPractice/app.js
@@ -1,3 +1,8 @@
 //'use strict';
 
 console.log('Hi there!');
+
+// Found out about the set timeout function at: https://stackoverflow.com/questions/18503001/node-js-console-gets-closed-immediately-after-i-execute-the-program-from-visual
+setTimeout(function () {
+    process.exit();
+}, 5000);
